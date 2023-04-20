814c66676cc8300ceac42c3c334e8e72b72d3b5e
David Song <davsong@cs.washington.edu>
2023-04-20 14:29:41 -0700

    wrote content script to get data from registration and wrote code that should save state idk if it works tho

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,44 @@
 // Get the content from class schedule.
+function getClassSchedule() {
+    const table = document.getElementsByClassName("sps_table even_rows").rows;
+    if (table) {
+        let map = {}
+    
+        // Loop over each course
+        table.forEach(tr => {
+            let sln = tr.cells[0] // SLN
+            let title =  tr.cells[1] // Course Title; CSE 403
+            let type = tr.cells[2] // LC = lecture; QZ = section; IS = individual
+            let name = tr.cells[4] // Course Name; Software Engineering
+            let days = tr.cells[5] // Days
+            let time = tr.cells[6] // Time
+            let location = tr.cells[7] // Location
+            let prof = tr.cells[8] // Instructor
+    
+            map[title] = {
+                          "sln": sln, 
+                          "title": title, 
+                          "type": type, 
+                          "name": name, 
+                          "days": days, 
+                          "time": time, 
+                          "location": location, 
+                          "prof": prof
+                        }
+        })
+        return map
+    }
+    else {
+        alert("No class schedule detected")
+        return null
+    }
+}
+
+// Gets the quarter. In form of "Spring 2023"
+function getQuarter() {
+    return document.getElementsByTagName("h1")[0].innerText
+}
+
+function getSections(map) {
+    
+}
