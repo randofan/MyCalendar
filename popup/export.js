f34c36830f54ec97059324eed36733b72474d5d2
thcho <thcho@uw.edu>
2023-04-24 15:53:00 -0700

    added some scaffolding to export.js

---
+++ popup/export.js
@@ -0,0 +1,16 @@
+var selection = {
+    // fields are hardcoded for sake of example
+    schedule: ["CSE 403", "CSE 340"],
+    sections: ["CSE 340"]
+}
+
+function displayNames() {
+    // get course names from data scraped by content.js and populate selection
+    // populate HTML with courses + input checkbox elements
+}
+
+function onClick() {
+    // travserse HTML checkbox input elements and populate selection accordingly
+    // to send to ics.js
+}
+
