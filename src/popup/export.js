0f5de96cb06911daacee8894fd187b1076090602
thcho <thcho@uw.edu>
2023-04-30 00:16:12 -0700

    implemented download button handler

--- src/popup/export.js
+++ src/popup/export.js
@@ -1,19 +1,18 @@
-// var selection = {
-//     // fields are hardcoded for sake of example
-//     schedule: ["CSE 403", "CSE 340"],
-//     sections: ["CSE 340"]
-// }
-var selection = {
+// e.g. Populated selection
+//      schedule: ["CSE 403", "CSE 340"],
+//      sections: ["CSE 340"]
+export var selection = {
     schedule: [],
     sections: []
 }
 
+// TODO: populate courses from content.js. gi
 var courses;
 
-// get course names from data scraped by content.js and populate selection
+// Get course names from data scraped by content.js and populate selection
 // populate HTML with courses + input checkbox elements
 function displayNames() {
-    // TODO: populate courses from content.js. gi
+    // TEMPORARILY HARD CODED
     courses = ["CSE 403", "CSE 340", "HCDE 318"];
 
     let table = document.querySelector('.selection-table');
@@ -34,9 +33,21 @@ function displayNames() {
     }
 }
 
-// travserse HTML checkbox input elements and populate selection accordingly
+// Travserse HTML checkbox input elements and populate selection accordingly
 // to send to ics.js
 function onDownloadClick() {
+    const inputs = document.getElementsByTagName("tr"); // returns an HTMLCollection, NOT an array
 
+    for (let i = 1; i < inputs.length; i++) { // omit the table header (first element)
+        let input = inputs[i];
+        const cells = input.getElementsByTagName("th");
+
+        const course = cells[0].innerHTML;
+        const export_schedule = cells[1].getElementsByTagName("input")[0].checked;
+        const export_sections = cells[2].getElementsByTagName("input")[0].checked;
+
+        // Push course name to respective arrays based on if user checked the box
+        if (export_schedule) { selection.schedule.push(course); };
+        if (export_sections) { selection.sections.push(course); };
+    }
 }
-
