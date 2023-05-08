3c2bd49c322c1f796d8ef843307cc95125e31c23
David Song <davsong@cs.washington.edu>
2023-05-08 13:22:07 -0700

    setup

--- test/setup.sh
+++ test/setup.sh
@@ -1,4 +1,7 @@
 cp -r ../src .
+echo "" >> src/popup/export.js
 echo "module.exports = displayNames" >> src/popup/export.js
+echo "" >> src/popup/ics.js
 echo "module.exports = buildICS" >> src/popup/ics.js
+echo "" >> src/scripts/content.js
 echo "module.exports = getClassSchedule" >> src/scripts/content.js
