7f978b895a3b4deb8ab2e5269b1f6e9b6c49b886
David Song <davsong@cs.washington.edu>
2023-05-16 13:47:40 -0700

    moved request to own file

--- src/popup/export.js
+++ src/popup/export.js
@@ -12,24 +12,6 @@ var scheduleData = null;
 })();
 
 /**
- * Wrapper to GET a webpage. Note, you must add every url you GET from
- * to manifest.json -> "host_permissions".
- * Unable to unit test.
- * 
- * @param {*} url 
- * @returns 
- */
-const getRequest = (url) => {
-    let res = {}
-    const urls = [url]
-    urls.forEach(async (url) => {
-        const r = await chrome.runtime.sendMessage({url: url})
-        res['body'] = r.page
-    });
-    return res
-}
-
-/**
  * Get course names from data scraped by content.js and populate selection
  * populate HTML with courses + input checkbox elements.
  * Unable to unit test.
