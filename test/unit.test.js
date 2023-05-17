f17e8beead71feba2e5d31411f5fe8108d84fe6c
thcho <thcho@uw.edu>
2023-05-16 17:57:20 -0700

    add table html generator unit test for export.js

--- test/unit.test.js
+++ test/unit.test.js
@@ -1,4 +1,5 @@
 const {convertDays, convertTime} = require('./src/popup/ics.js');
+const {generateTableRow} = require('./src/popup/export.js');
 const assert = require('assert');
 
 describe('ICS Unit Tests', () => {
@@ -36,5 +37,22 @@ describe('ICS Unit Tests', () => {
     registrationTime = " 630- 720";
     answer = convertTime(registrationTime);
     assert.deepEqual(answer, ["183000", "192000"]);
+  });
+});
+
+describe('Export Unit Tests', () => {
+  it('tests HTML for table row generator', () => {
+    let course = "CSE 403";
+    let id_schedule = "CSE-403-schedule";
+    let id_section = "CSE-403-section";
+
+    let expected_html = '<tr>' +
+                        `    <th class="course-name">${course}</th>` +
+                        `    <th><input type="checkbox" id=${id_schedule} value="schedule"></th>` +
+                        `    <th><input type="checkbox" id=${id_section} value="section" disabled></th>` +
+                        '</tr>'
+
+    let html = generateTableRow(course);
+    assert.deepEqual(html, expected_html);
   });
 });
