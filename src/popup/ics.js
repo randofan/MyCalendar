17231a81e0a8f889e32bfd7a419e2653a8e66f7a
Gavin LaMotte <glamot@cs.washington.edu>
2023-05-23 20:31:42 -0700

    fixed the map link in ics

--- src/popup/ics.js
+++ src/popup/ics.js
@@ -85,7 +85,7 @@ function buildICS(scheduleData, info, includeLink) {
             file += "RRULE:FREQ=WEEKLY;BYDAY=" + convertDays(en.days) + ";UNTIL=" + endDates[i] + "\n"; // change for holiday
             file += "LOCATION:" + en.location + "\n";
             if (includeLink) {
-                file += `DESCRIPTION:<HTML><BODY><a href=${en.link}>Building directions</a></BODY></HTML>` + "\n";
+                file += `DESCRIPTION:${en.link}` + "\n";
             }
             file += "END:VEVENT\n";
         }
